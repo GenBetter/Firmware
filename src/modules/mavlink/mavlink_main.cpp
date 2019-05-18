4f075505c322c39060d4e8a4fde257d3a3f15227
wanggen123 <1254281752@qq.com>
2019-05-18 11:19:32 +0800

    精简一些mavlink数据

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1970,44 +1970,44 @@ Mavlink::task_main(int argc, char *argv[])
 	switch (_mode) {
 	case MAVLINK_MODE_NORMAL:
 		configure_stream("SYS_STATUS", 1.0f);
-		configure_stream("EXTENDED_SYS_STATE", 1.0f);
+		//configure_stream("EXTENDED_SYS_STATE", 1.0f);
 		configure_stream("HIGHRES_IMU", 1.5f);
 		configure_stream("ATTITUDE", 20.0f);
 		configure_stream("RC_CHANNELS", 5.0f);
 		configure_stream("SERVO_OUTPUT_RAW_0", 1.0f);
 		configure_stream("ALTITUDE", 1.0f);
-		configure_stream("GPS_RAW_INT", 1.0f);
-		configure_stream("ADSB_VEHICLE", 2.0f);
-		configure_stream("COLLISION", 2.0f);
+		//configure_stream("GPS_RAW_INT", 1.0f);
+		//configure_stream("ADSB_VEHICLE", 2.0f);
+		//configure_stream("COLLISION", 2.0f);
 		configure_stream("DISTANCE_SENSOR", 0.5f);
-		configure_stream("OPTICAL_FLOW_RAD", 1.0f);
-		configure_stream("VISION_POSITION_NED", 1.0f);
+		//configure_stream("OPTICAL_FLOW_RAD", 1.0f);
+		//configure_stream("VISION_POSITION_NED", 1.0f);
 		configure_stream("ESTIMATOR_STATUS", 0.5f);
 		configure_stream("NAV_CONTROLLER_OUTPUT", 1.5f);
-		configure_stream("GLOBAL_POSITION_INT", 5.0f);
+		//configure_stream("GLOBAL_POSITION_INT", 5.0f);
 		configure_stream("LOCAL_POSITION_NED", 1.0f);
 		configure_stream("POSITION_TARGET_GLOBAL_INT", 1.5f);
 		configure_stream("ATTITUDE_TARGET", 2.0f);
 		configure_stream("HOME_POSITION", 0.5f);
-		configure_stream("NAMED_VALUE_FLOAT", 1.0f);
-		configure_stream("VFR_HUD", 4.0f);
-		configure_stream("WIND_COV", 1.0f);
+		// configure_stream("NAMED_VALUE_FLOAT", 1.0f);
+		// configure_stream("VFR_HUD", 4.0f);
+		// configure_stream("WIND_COV", 1.0f);
 		break;
 
 	case MAVLINK_MODE_ONBOARD:
 		configure_stream("SYS_STATUS", 1.0f);
-		configure_stream("EXTENDED_SYS_STATE", 2.0f);
+		//configure_stream("EXTENDED_SYS_STATE", 2.0f);
 		configure_stream("HIGHRES_IMU", 50.0f);
 		configure_stream("ATTITUDE", 250.0f);
-		configure_stream("RC_CHANNELS", 20.0f);
+		//configure_stream("RC_CHANNELS", 20.0f);
 		configure_stream("SERVO_OUTPUT_RAW_0", 10.0f);
 		configure_stream("ALTITUDE", 10.0f);
-		configure_stream("GPS_RAW_INT", 5.0f);
-		configure_stream("ADSB_VEHICLE", 10.0f);
-		configure_stream("COLLISION", 10.0f);
-		configure_stream("DISTANCE_SENSOR", 10.0f);
-		configure_stream("OPTICAL_FLOW_RAD", 10.0f);
-		configure_stream("VISION_POSITION_NED", 10.0f);
+		// configure_stream("GPS_RAW_INT", 5.0f);
+		// configure_stream("ADSB_VEHICLE", 10.0f);
+		// configure_stream("COLLISION", 10.0f);
+		// configure_stream("DISTANCE_SENSOR", 10.0f);
+		// configure_stream("OPTICAL_FLOW_RAD", 10.0f);
+		// configure_stream("VISION_POSITION_NED", 10.0f);
 		configure_stream("ESTIMATOR_STATUS", 1.0f);
 		configure_stream("NAV_CONTROLLER_OUTPUT", 10.0f);
 		configure_stream("GLOBAL_POSITION_INT", 50.0f);
@@ -2015,15 +2015,15 @@ Mavlink::task_main(int argc, char *argv[])
 		configure_stream("POSITION_TARGET_GLOBAL_INT", 10.0f);
 		configure_stream("ATTITUDE_TARGET", 10.0f);
 		configure_stream("HOME_POSITION", 0.5f);
-		configure_stream("NAMED_VALUE_FLOAT", 10.0f);
-		configure_stream("VFR_HUD", 10.0f);
-		configure_stream("WIND_COV", 10.0f);
+		// configure_stream("NAMED_VALUE_FLOAT", 10.0f);
+		// configure_stream("VFR_HUD", 10.0f);
+		// configure_stream("WIND_COV", 10.0f);
 		configure_stream("POSITION_TARGET_LOCAL_NED", 10.0f);
 		configure_stream("SYSTEM_TIME", 1.0f);
-		configure_stream("TIMESYNC", 10.0f);
-		configure_stream("CAMERA_CAPTURE", 2.0f);
+		// configure_stream("TIMESYNC", 10.0f);
+		// configure_stream("CAMERA_CAPTURE", 2.0f);
 		//camera trigger is rate limited at the source, do not limit here
-		configure_stream("CAMERA_TRIGGER", 500.0f);
+		// configure_stream("CAMERA_TRIGGER", 500.0f);
 		configure_stream("ACTUATOR_CONTROL_TARGET0", 10.0f);
 		break;
 
